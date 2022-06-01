2406fd71b3ed5d25860e601e8da9872999d272b6
Etibarsaidbayli <etibarseidbekov@gmail.com>
2022-06-01 21:25:29 +0300

    bug fixing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,180 +1,174 @@
-let form = document.getElementById("form");
-let addMessage = document.getElementById("todo-input");
-let wrapper = document.querySelector(".todo-items");
-const inputCheck = document.getElementById("inputCheck");
-const itemsCount = document.getElementById("itemsCount");
-const completedSpan = document.getElementById("completedSpan");
+const addMessage = document.getElementById("todo-input");
+const form = document.getElementById("form");
+const wrapper = document.querySelector(".todo-items");
+let inputCheck = document.getElementById("inputCheck");
+let itemsCount = document.getElementById("itemsCount");
+
 const allSpan = document.getElementById("all");
-const activeSpan = document.getElementById("activeSpan");
-const allCompletedDelete = document.querySelector(".items-clear");
 let todoList = [];
+const itemStatuses = document.querySelector(".items-statuses");
+const activeSpan = document.querySelector(".active");
+const completedSpan = document.querySelector(".completed");
+const itemsClear = document.querySelector(".items-clear");
+const itemsLeft = document.querySelector(".items-left");
+itemsCount.textContent = 0;
 
-//arr for completed task
-let todoItemElems = [];
+form.addEventListener("submit", addTodo);
+wrapper.addEventListener("click", deleteCheck);
+allSpan.addEventListener("click", filterAllTodo);
+activeSpan.addEventListener("click", filterActiveTodo);
+completedSpan.addEventListener("click", filterCompletedSpan);
 
-form.addEventListener("submit", function (event) {
+itemsClear.addEventListener("click", clearCompleted);
+
+function addTodo(event) {
   event.preventDefault();
-
   if (!addMessage.value.trim()) return;
-  let newTodo = {
-    text: addMessage.value,
-    completed: inputCheck.checked,
-  };
-  todoList.unshift(newTodo);
-  fillHtml();
+
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo-item");
+  if (inputCheck.checked) {
+    todoDiv.classList.add("checked");
+    itemsCount.textContent--;
+  }
+
+  const todoCheckDiv = document.createElement("div");
+  todoCheckDiv.classList.add("check");
+  todoDiv.appendChild(todoCheckDiv);
+
+  const todoCheckMarkDiv = document.createElement("div");
+  todoCheckMarkDiv.classList.add("check-mark");
+  todoCheckDiv.appendChild(todoCheckMarkDiv);
+
+  const checkCompleteInp = document.createElement("input");
+  checkCompleteInp.classList.add("complete-btn");
+  checkCompleteInp.type = "checkbox";
+  checkCompleteInp.checked = inputCheck.checked;
+
+  todoCheckMarkDiv.appendChild(checkCompleteInp);
+
+  const todoTextDiv = document.createElement("div");
+  todoTextDiv.classList.add("todo-text");
+  todoTextDiv.innerHTML = addMessage.value;
+  todoCheckDiv.appendChild(todoTextDiv);
+
+  const trashBtn = document.createElement("button");
+  trashBtn.classList.add("trash");
+  trashBtn.innerHTML = "X";
+  todoDiv.appendChild(trashBtn);
+
+  wrapper.appendChild(todoDiv);
+
+  todoList.push(todoDiv);
+
   addMessage.value = "";
-  inputCheck.checked = false;
-});
 
-function fillHtml() {
-  wrapper.innerHTML = "";
-  if (todoList.length > 0) {
-    todoList.forEach((item, index) => {
-      wrapper.innerHTML += createHtml(item, index);
-    })
-    todoItemElems = document.querySelectorAll(".todo-item");
-  }
-
-  checkLength2();
-}
-fillHtml();
-
-function createHtml(task, index) {
-  return `<div class="todo-item ${task.completed ? "checked" : ""}">
-  <div class="check">
-      <div class="check-mark">
-          <input onclick="completeTask(${index})" type="checkbox" ${
-    task.completed ? "checked" : ""
-  } />
-      </div>
- <div class="todo-text">
-      ${todoList[index].text}
-  </div>
-  </div>
-  <button onclick="deleteTask(${index})"><i class="fa-solid fa-x"></i></button>
-</div>  `;
+  inputCheck.checked = "";
+  itemsLeft.classList.add("important");
+  itemsCount.textContent++;
 }
 
-function completeTask(index) {
-  todoList[index].completed = !todoList[index].completed;
-  if (todoList[index].completed) {
-    allCompletedDelete.classList.add("active");
-  } else if (!todoList[index].completed) {
-    allCompletedDelete.classList.remove("active");
+function deleteCheck(event) {
+  if (event.target.classList[0] === "trash") {
+    event.target.parentElement.classList.add("delition");
+    setTimeout(() => {
+      event.target.parentElement.remove();
+    }, 500);
+
+    itemsCount.textContent--;
   }
-  if (todoList[index].completed) {
-    todoItemElems[index].classList.add("checked");
-  } else {
-    todoItemElems[index].classList.remove("checked");
+  if (itemsCount.textContent <= 0) {
+    itemsCount.textContent = "0";
+    itemsLeft.classList.remove("important");
   }
-  fillHtml();
-}
 
-function deleteTask(index) {
-  todoItemElems[index].classList.add("delition");
-  setTimeout(() => {
-    todoList.splice(index, 1);
-    fillHtml();
-  }, 500);
-}
+  if (event.target.matches('input[type="checkbox"]')) {
+    const todoItem = event.target.closest(".todo-item");
+    todoItem.classList.toggle("checked");
 
-completedSpan.addEventListener("click", function () {
-  let b = todoList.filter((a) => a.completed);
-  if (b.length == 0) {
-    wrapper.innerHTML = `<p style='text-align:center;opacity:.3;padding:15px ';>Bitirdiyin ish yoxdu.Bekarciliqdi?</p>`;
-  }
-  let displayMes = "";
-  for (let i = 0; i < b.length; i++) {
-    displayMes += `<div class="todo-item ${"checked"}">
-     <div class="check">
-         <div class="check-mark">
-             <input onclick="completeTask(${i})" type="checkbox" ${"checked"} />
-         </div>
-          <div class="todo-text">
-         ${b[i].text}
-     </div>
-     </div>
-     <button onclick="deleteTask(${i})"><i class="fa-solid fa-x"></i></button>
- </div>`;
-    wrapper.innerHTML = displayMes;
-  }
-  activeSpan.classList.remove("active");
-  completedSpan.classList.add("active");
-});
-
-activeSpan.addEventListener("click", function () {
-  let actives = todoList.filter((a) => !a.completed);
-  let displayMesag = "";
-  for (let i = 0; i < actives.length; i++) {
-    displayMesag += `<div class="todo-item ">
-     <div class="check">
-         <div class="check-mark">
-             <input onclick="completeTask(${i})" type="checkbox"  />
-         </div>
-            <div class="todo-text">
-         ${actives[i].text}
-     </div>
-     </div>
-     <button onclick="deleteTask(${i})"><i class="fa-solid fa-x"></i></button>
- </div>`;
-    wrapper.innerHTML = displayMesag;
-  }
-  allSpan.classList.remove("active");
-  completedSpan.classList.remove("active");
-  activeSpan.classList.add("active");
-});
-
-allSpan.addEventListener("click", function () {
-  fillHtml();
-  activeSpan.classList.remove("active");
-  completedSpan.classList.remove("active");
-  allSpan.classList.add("active");
-});
-
-
-function checkLength2() {
-  itemsCount.textContent = todoList.length;
-  for (let i = 0; i < todoList.length; i++) {
-    if (todoList[i].completed) {
+    if (todoItem.classList.contains("checked")) {
       itemsCount.textContent--;
+      itemsClear.classList.add("important");
+    } else if (!todoItem.classList.contains("checked")) {
+      itemsCount.textContent++;
+      itemsClear.classList.remove("important");
     }
   }
 }
 
-allCompletedDelete.addEventListener("click", function () {
-  completeDelete();
-  fillHtml();
-});
+function filterAllTodo() {
+  const todos = wrapper.childNodes;
 
-function completeDelete() {
-  todoList = todoList.filter((item) => item.completed === false);
+  todos.forEach(function (item) {
+    item.style.display = "flex";
+  });
+  allSpan.classList.add("important");
+  activeSpan.classList.remove("important");
+  completedSpan.classList.remove("important");
 }
 
-const themeBtn = document.querySelector(".theme");
+function filterActiveTodo() {
+  const todos = wrapper.childNodes;
 
-let bgColor = document.querySelector("#bgColor");
-
-function ligthModeOn() {
-  bgColor.src = "assets/img/bg-desktop-light.jpg";
+  todos.forEach(function (item) {
+    if (!item.classList.contains("checked")) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+  allSpan.classList.remove("important");
+  activeSpan.classList.add("important");
 }
-function darkMode() {
-  bgColor.src = "assets/img/bg-desktop-dark.jpg";
-};
+
+function filterCompletedSpan() {
+  const todos = wrapper.childNodes;
+
+  todos.forEach(function (item) {
+    if (item.classList.contains("checked")) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+  allSpan.classList.remove("important");
+  activeSpan.classList.remove("important");
+  completedSpan.classList.add("important");
+}
+
+function clearCompleted() {
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].classList.contains("checked")) {
+      todoList[i].classList.add("delition");
+      setTimeout(() => {
+        todoList[i].remove();
+      }, 500);
+    }
+  }
+}
 
 let sun = document.getElementById("sun");
-
+let bgColor = document.getElementById("bgColor");
 let theme = "light";
+
+function lightModeOn() {
+  bgColor.src = "assets/img/bg-desktop-light.jpg";
+}
+
+function darkModeOn() {
+  bgColor.src = "assets/img/bg-desktop-dark.jpg";
+}
+
 function transformSunToMoon() {
   if (theme === "light") {
     sun.src = "assets/img/icon-moon.svg";
-    darkMode();
+    darkModeOn();
     theme = "dark";
   } else {
     sun.src = "assets/img/icon-sun.svg";
-    ligthModeOn();
+    lightModeOn();
     theme = "light";
   }
 }
 
 sun.addEventListener("click", transformSunToMoon);
-
